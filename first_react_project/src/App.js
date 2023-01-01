f98c26587f02778512566c0581ba336abf0bbd38
ohinhyuk <8156217@naver.com>
2023-01-01 19:13:50 +0900

    react project
    react practice
    
    using coin api

--- first_react_project/src/App.js
+++ first_react_project/src/App.js
@@ -3,39 +3,47 @@ import Button from "./Button";
 
 function App() {
 
-  const [value,setValue] = useState("");
-  const [toDos , setToDos] = useState([]);
+  const [coins , setCoins] = useState("");
+  const [load, setLoad] = useState(true);
+  const [money , setMoney] = useState(0);
+  const [coinOne , setCoinOne] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoad(false);
+      console.log(json)
+    })
+  }
+  , [])
+
+  const onCoinChange = (event) =>{    
+    setCoinOne(event.target.value);
+  }
+
 
   const onChange = (event) =>{
-    setValue(event.target.value);
+    setMoney(event.target.value);
   }
-  const onSubmit = (event) =>{
-    event.preventDefault();
-    if(value !== ""){
-      setToDos(currentToDos => [ value, ...currentToDos]);  
-      setValue("");
-    }
-  }
-
-  const handleDelete = (idx) => {
-    // const li = event.target.parentElement;
-    // console.log(li);
-    setToDos(prev => prev.filter((item,index) => index !== idx ));
-    
-  }
-  // console.log(toDos);
-  useEffect(()=>console.log(toDos),[toDos]);
+  
   return (
+    
     <div>
-      <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={value} placeholder="please enter your toDo" />
-      <button>Submit</button>
-      </form>
-      <ul>
-        {toDos.map((item,index) => (
-          <li key={index}>{item}<button onClick={ () => handleDelete(index)}>X</button></li>
-          ))}
-      </ul>
+      <h1>Coins! {load ? "" : `( ${coins.length} 개)`}</h1>
+      
+    
+      {load ? <h3>Loading...!</h3> : null}
+      <select onChange={onCoinChange}> 
+      {load ? null : coins.map((coin,index) => 
+      (<option value={coin.quotes.USD.price} key={coin.id}>{coin.name} {coin.quotes.USD.price}</option>) 
+        
+        )
+        }
+      </select>
+      <input type="number" value={money} onChange={onChange} placeholder="please input your money!"></input>
+      <h3>{parseInt(money) * parseInt(coinOne) }</h3>
     </div>
 
   );
