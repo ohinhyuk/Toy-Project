d5a954c195f6c6e4b648d383e7eca233c9e16fed
ohinhyuk <8156217@naver.com>
2023-01-18 00:09:44 +0900

    React study
    studying about Hooks

---
+++ reactProject_webService/movie/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './saved/App';
+// import Coin from './Coin';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+    // <Coin />
+);
---
+++ reactProject_webService/src/useTab.js
@@ -0,0 +1,11 @@
+const useTabs = (initialValue, allTabs) => {
+    const [currentIndex , setCurrentIndex] = useState(initialValue);
+    if(!allTabs || !Array.isArray(allTabs)){
+      return;
+    }
+    
+    return{
+      currentItem : allTabs[currentIndex],
+      changeItem : setCurrentIndex
+    };
+  };
