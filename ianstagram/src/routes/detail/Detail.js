5fd0b88fe5c9f613730f29b10e4ab08a59c7dc82
ohinhyuk <8156217@naver.com>
2023-01-22 02:57:02 +0900

    ianstagram detail page
    outline complete

--- ianstagram/src/routes/detail/Detail.js
+++ ianstagram/src/routes/detail/Detail.js
@@ -1,9 +1,16 @@
+import { Box } from "@mui/system";
 import React from "react";
 
 function Detail(){
 
+    document.body.style = 'background: rgba(0, 0, 0, 0.5);';
+
     return(
-        <div>Detail</div>
+        <Box sx={{width:"85%" , height:"95vh", mt:2, marginX:"auto" , display:'flex' }}>
+            <Box sx={{ height:"100%", width:"60%", border:0, backgroundColor:"black"}}></Box>
+            <Box sx={{ height:"100%",width:"40%",border:0, backgroundColor:"white"}}></Box>
+            
+        </Box>
     );
 }
 
