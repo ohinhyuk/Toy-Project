d5fa5e33cf9b2c2417e616c4f57dcd39e3dd5e97
ohinhyuk <8156217@naver.com>
2023-01-22 02:49:19 +0900

    ianstagram
    story outline complete

--- ianstagram/src/routes/story/Story.js
+++ ianstagram/src/routes/story/Story.js
@@ -1,9 +1,27 @@
+import { Box, createTheme, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
+
+const headerTheme = createTheme({
+    typography: {
+        fontFamily: "'Dancing Script', cursive",
+        fontSize : 15,
+    },
+})
 
 function Story(){
 
+    document.body.style = 'background: rgba(0, 0, 0, 0.9);';
+
     return(
-        <div>Story</div>
+        <Box>
+            <ThemeProvider theme={headerTheme}>
+            <Typography variant="h6" sx={{color:"white" , pl : 2}}>Ianstagram</Typography>
+            </ThemeProvider>
+
+            <Box sx={{ width:"30%" , height:"90vh" , margin:"auto" , border:1 , borderColor:"gray" , backgroundColor:"gray"}}>
+
+            </Box>
+        </Box>
     );
 }
 
