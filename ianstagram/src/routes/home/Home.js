5b0262598503822c9ed9b4013ea5e8a8ef427a99
ohinhyuk <8156217@naver.com>
2023-01-21 15:56:27 +0900

    Ianstagram : Main page
    Home.js -> Main page
    
    Grid complete

--- ianstagram/src/routes/home/Home.js
+++ ianstagram/src/routes/home/Home.js
@@ -1,40 +1,44 @@
+import { makeStyles } from "@material-ui/core";
 import { Box, Grid, Paper } from "@mui/material";
-import { display } from "@mui/system";
+import { createTheme, display, ThemeProvider } from "@mui/system";
 import React from "react";
+const gridStyles = makeStyles((theme) => ({
+    root: {
+        // padding: theme.spacing(1),
+        [theme.breakpoints.down('xs')]: {
+            gridTemplateAreas: `"story"
+                            "main"`,
+            gridTemplateColumns: "100%" 
+        },
+        [theme.breakpoints.up('sm')]:{
+            gridTemplateAreas: `"story"
+                            "main"`,
+            gridTemplateColumns: "100%" 
+        },
+        [theme.breakpoints.up('md')]:{
+            gridTemplateAreas: `"story sidebar"
+                            "main sidebar"`,
+            gridTemplateColumns: "minmax(0,2fr) minmax(0,1fr)"  
+        },
+    }
+}));
 
 function Home(){
-
-    const styles = theme => ({
-        root: {
-            padding: theme.spacing(1),
-            [theme.breakpoints.down('xs')]: {
-                gridTemplateAreas: `"story"
-                                "main"`,
-                gridTemplateColumns: "minmax(0,1fr)" 
-            },
-            [theme.breakpoints.up('sm')]:{
-                gridTemplateAreas: `"story"
-                                "main"`,
-                gridTemplateColumns: "minmax(0,1fr)" 
-            },
-            [theme.breakpoints.up('md')]:{
-                gridTemplateAreas: `"story sidebar"
-                                "main sidebar"`,
-                gridTemplateColumns: "minmax(0,2fr) minmax(0,1fr)"  
-            },
-        },
-    });
+    const classes = gridStyles();    
     
     return(
-        <Box styles={styles}  sx={{
+        
+        <Box component="div"  sx={{
             display: "grid",
             gap: "3rem",
-            width:"50%",
+            width:"60%",
             margin: "auto",
+            
             // gridTemplateAreas: `"story sidebar"
             //                     "main sidebar"`,
             // gridTemplateColumns: "minmax(0,2fr) minmax(0,1fr)"                 
-           }}>
+           }}
+           className={classes.root}>
                 
                 
                 
@@ -54,6 +58,7 @@ function Home(){
 
                 </Paper>
         </Box>
+        
         
     );
 }
