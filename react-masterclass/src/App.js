9771371c4169116c8a087057f7f603bb99b0b8ec
ohinhyuk <8156217@naver.com>
2023-01-30 14:24:48 +0900

    study : styled-component
    as , attrs

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -15,12 +15,26 @@ const Circle = styled(Box)`
   border-radius: 50%;
 `;
 
+const Text = styled.span`
+color : white;
+margin-left: 50px;
+`
+
+const AttrsText = styled(Text).attrs({ href : "www.naver.com"})`
+  margin-left : 0px
+`
+
 
 function App(){
   return (
     <Father >
-      <Box bgColor="tomato"/>
-      <Circle bgColor="tomato"/>
+      <Box bgColor="teal">
+        <Text>T1</Text>
+      </Box>
+      <Circle bgColor="tomato">
+        <Text as="a" href="/">T2</Text>
+        <AttrsText as="a"> attrs Applied</AttrsText>
+      </Circle>
     </Father>
   );
 }
