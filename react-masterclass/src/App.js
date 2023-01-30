b82f280343c36e40066596ffb338e8d38827321b
ohinhyuk <8156217@naver.com>
2023-01-30 14:10:59 +0900

    Study
    styled component ->props + extension

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
-function App() {
+const Father = styled.div`
+  display : flex;
+`;
+
+const Box = styled.div`
+  background-color : ${(props) => props.bgColor };
+  width : 100px;
+  height : 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
+
+
+function App(){
   return (
-    <div>
-      App
-    </div>
+    <Father >
+      <Box bgColor="tomato"/>
+      <Circle bgColor="tomato"/>
+    </Father>
   );
 }
 
 export default App;
+
