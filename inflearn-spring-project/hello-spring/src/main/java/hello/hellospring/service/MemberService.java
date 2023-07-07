687072337cd2e6a435bae2992909025da4bbb6ba
ohinhyuk <8156217@naver.com>
2023-07-07 20:01:57 +0900

    spring 입문 인프런 강의 파이널 코드 업로드

--- inflearn-spring-project/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ inflearn-spring-project/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,26 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+
+//@Service
+@Transactional
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-        
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+//    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
     public Long join(Member member){
         validateDuplication(member);
 
@@ -25,11 +37,11 @@ public class MemberService {
                 });
     }
 
-    private List<Member> findMembers(){
+    public List<Member> findMembers(){
         return memberRepository.findAll();
     }
 
-    private Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
     }
 
