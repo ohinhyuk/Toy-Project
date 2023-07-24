8522c3364baa2cf78ca213ec42167bb4ca4306fe
ohinhyuk <8156217@naver.com>
2023-07-24 17:11:10 +0900

    study : JPA rest api 성능 최적화 fetch join + DTO

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -58,6 +58,29 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" , Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem(){
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d"+
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
 //    public List<Order> findAll(OrderSearch orderSearch){
 //        QOrder order = QOrder.order;
 //        QMember member = QMember.member;
